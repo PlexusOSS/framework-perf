a931a3479fef225691151a4e6300eca47d524564
Mark George <mark.george@plexusworldwide.com>
2018-11-15 10:14:51 -0700

    Creating hapi server

---
+++ hapi.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi    = require('hapi');
+const server  = new Hapi.server({
+  host: 'localhost',
+  port: 3000
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  config: {
+    handler: (request, h) => {
+      return h.response({
+        msg: 'Hello Human!'
+      }).code(200);
+    }
+  }
+});
+
+const init = async () => {
+  await server.start();
+  console.log('Hapi server started on port 3000...');
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
